ff90e80bfccf330d41af30cba493691bde98f7ff
z-invernus <invernus0513@gmail.com>
2021-05-20 21:18:24 +0200

    g

--- index.js
+++ index.js
@@ -7,8 +7,26 @@ const command = require('./command')
 client.on('ready', () => {
     console.log('Moderation successfully started.')
 
-    command(client, 'ping', message => {
+    command(client, ['ping', 'pong'], message => {
         message.channel.send('Pong!')
+    })
+    command(client, ['clear', 'clearchannel', 'cc', 'nuke'], (message) => {
+        if (message.member.hasPermission('ADMINISTRATOR')) {
+            message.channel.messages.fetch().then((results) => {
+                message.channel.bulkDelete(results)
+            })
+        }
+    })
+    command(client, 'set-status', (message) => {
+        const content = message.content.replace('s!set-status ', '')
+        if (message.member.hasPermission('ADMINISTRATOR')) {
+            client.user.setPresence({
+                activity: {
+                    name: content,
+                    type: 0,
+                }
+            })
+        }
     })
 })
 
